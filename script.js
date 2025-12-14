0b60562be38635d2f3fbf639c87e7119394cad2d
Aashi Guleria <guleriaaashi@gmail.com>
2025-12-14 12:25:42 +0530

    successfully

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling
+const links = document.querySelectorAll('.nav a');
+
+
+links.forEach(link => {
+link.addEventListener('click', e => {
+e.preventDefault();
+document.querySelector(link.getAttribute('href')).scrollIntoView({
+behavior: 'smooth'
+});
+});
+});
